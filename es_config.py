7131f7a18870cd0192bf94d01360ea7bfbe3dc11
Timothee Besset <ttimo@ttimo.net>
2013-09-17 09:13:38 -0500

    better default configuration setup

--- es_config.py
+++ es_config.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
+import os, pprint
+
+_TOP_DIR = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '..' ) )
+SOURCE_DIR = os.path.join( _TOP_DIR, 'source' )
+BUILD_DIR = os.path.join( _TOP_DIR, 'build' )
+INSTALL_DIR = os.path.join( _TOP_DIR, 'run' )
+
 PACKAGES_DIR='packages'
-SOURCE_DIR='/opt/source/'
-BUILD_DIR='/opt/build/'
-INSTALL_DIR='/opt/run/'
+
+if ( __name__ == '__main__' ):
+    # if executed directly, print the configuration as we see it
+    for ( name, value ) in sorted( globals().items() ):
+        if ( not name[0].isupper() ):
+            continue
+        print( '%s=%s' % ( name, repr( value ) ) )
+
+
