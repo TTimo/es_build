6696e9a15c71000d280e6af65204e7dbbeea1905
Tomasz Kruk <unhitpl@gmail.com>
2013-09-28 22:24:36 +0200

    Conditional autogen.sh

--- packages/10_libzmq.py
+++ packages/10_libzmq.py
@@ -16,10 +16,14 @@ class PackageDetails( package_helpers.PackageTemplate ):
         assert( os.path.exists( package_source_dir ) )
         package_build_dir = os.path.join( build_dir, self.dirname )
         sh.mkdir( '-p', package_build_dir )
-	sh.cd( package_source_dir )
-	subprocess.check_call( [ os.path.join( package_source_dir, 'autogen.sh' ) ], shell = True ), 
+
+        # Invoke autogen.sh if configure script doesn't exist
+        if not os.path.exists( '%s%s%s' % ( package_source_dir, os.sep, 'configure' ) ):
+            sh.cd( package_source_dir )
+            subprocess.check_call( [ os.path.join( package_source_dir, 'autogen.sh' ) ], shell = True ),
+
         # NOTE: there are problems with cmake support on this still
-	sh.cd( package_build_dir )
+        sh.cd( package_build_dir )
         subprocess.check_call( [ os.path.join( package_source_dir, 'configure' ), '--prefix=%s' % install_dir ], shell = True )
         sh.make( '-j4', _out = sys.stdout )
         sh.make.install( _out = sys.stdout )
--- packages/20_czmq.py
+++ packages/20_czmq.py
@@ -16,9 +16,13 @@ class PackageDetails( package_helpers.PackageTemplate ):
         assert( os.path.exists( package_source_dir ) )
         package_build_dir = os.path.join( build_dir, self.dirname )
         sh.mkdir( '-p', package_build_dir )
-        sh.cd( package_source_dir )
-	subprocess.check_call( [ os.path.join( package_source_dir, 'autogen.sh') ], shell = True )
-	sh.cd( package_build_dir )
+
+        # Invoke autogen.sh if configure script doesn't exist
+        if not os.path.exists( '%s%s%s' % ( package_source_dir, os.sep, 'configure' ) ):
+            sh.cd( package_source_dir )
+            subprocess.check_call( [ os.path.join( package_source_dir, 'autogen.sh') ], shell = True )
+
+        sh.cd( package_build_dir )
         subprocess.check_call( [ os.path.join( package_source_dir, 'configure' ), '--prefix=%s' % install_dir, '--with-libzmq=%s' % install_dir ], shell = True )
         sh.make( '-j4', _out = sys.stdout )
         sh.make.install( _out = sys.stdout )
