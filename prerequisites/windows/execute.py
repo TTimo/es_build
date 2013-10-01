91b946ad1f26dbafc8c7bba9cfa7a7a2cb2ad7b5
TTimo <ttimo@ttimo.net>
2013-10-01 18:54:59 +0000

    install pip, s3cmd. prep for cmake. sh not supported on windows :(

--- prerequisites/windows/execute.py
+++ prerequisites/windows/execute.py
@@ -1,8 +1,12 @@
 # this is either executed manually, or by the powershell bootstrap
 
-import subprocess
+import sys, os, subprocess
 
 if ( __name__ == '__main__' ):
+    # setup current directory
+    os.chdir( os.path.abspath( os.path.dirname( __file__ ) ) )
+    print( 'Entering %s in %s' % ( __file__, os.getcwd() ) )
+
     # continue installation of system packages with Chocolatey
     for package in [
 	'VisualStudio2012WDX',
@@ -12,13 +16,30 @@ if ( __name__ == '__main__' ):
 	'AWSTools.Powershell',
 	'Wget',
 	'emacs',
-	'GoogleChrome' ]:
+	'GoogleChrome',
+    ]:
         subprocess.check_call( [ r'C:\Chocolatey\bin\chocolatey.bat', 'install', package ] )
 
-    # TODO: install a recent enough cmake
+    # install pip
+    if ( os.path.exists( 'ez_setup.py' ) ):
+        os.unlink( 'ez_setup.py' )
+    subprocess.check_call( [ r'C:\Chocolatey\bin\wget.bat', '--no-check-certificate', 'https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py' ] )
+    subprocess.check_call( [ 'python', 'ez_setup.py' ] )
 
-    # TODO: install pip and setuptools
-    # see http://stackoverflow.com/questions/4750806/how-to-install-pip-on-windows
+    if ( os.path.exists( 'get-pip.py' ) ):
+        os.unlink( 'get-pip.py' )
+    subprocess.check_call( [ r'C:\Chocolatey\bin\wget.bat', '--no-check-certificate', 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py' ] )
+    subprocess.check_call( [ 'python', 'get-pip.py' ] )
 
-    # TODO: leverage pip to install the latest sh and s3cmd
+    # TODO: add C:\Python27\Script to the system wide path if needed
+
+    # now that we have pip, use it
+    subprocess.check_call( [ r'C:\Python27\Scripts\pip.exe', 'install', '-U', '--pre', 's3cmd' ] )
+    # ouch, sh is not supported on Windows ..
+    # https://github.com/amoffat/sh/issues/155
+#    subprocess.check_call( [ r'C:\Python27\Scripts\pip.exe', 'install', '-U', 'sh' ] )
+
+    # install a recent enough cmake
+    subprocess.check_call( [ r'C:\Chocolatey\bin\wget.bat', 'http://www.cmake.org/files/v2.8/cmake-2.8.11.2-win32-x86.zip' ] )
+    # TODO: expand it to a top path, setup system wide path if needed
 
